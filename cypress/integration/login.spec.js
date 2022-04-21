43e1c39dfdee586ec0830bd1118fe78accf66c27
AAB-87 <andrej-abrekov@yandex.ru>
2022-04-21 10:32:56 +0400

    completed task 2

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -16,7 +16,7 @@ describe("When user is on login page, user", () => {
     it("Should not be able to login with empty email", () => {
         cy.visit("/");
         cy.login(" ", "test"); // кастомная команда
-        // cy.get('#mail').should("have class", ":invalid"); // у любого пустого поля которое должно быть заполнено перед логином 
+        // cy.get('#mail').should("have.class", ":invalid"); // у любого пустого поля которое должно быть заполнено перед логином 
         // появляется класс invalid по умолчанию. Если не срабатывает то это псевдокласс не является классом в элементе.
         cy.get('#mail').then($el => cy.log($el));
         cy.get('#mail').then($el => $el[0].checkValidity()).should('be.false'); // get-запрос вернул return (какой-то массив элементов), 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,8 +25,25 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('login', (email, password) => {
-    cy.contains("Log in").click();
+    cy.contains('Log in').click();
     cy.get('#mail').type(email);
     cy.get('#pass').type(password);
+    cy.contains('Submit').click();
+})
+
+Cypress.Commands.add('bookDescription', (title, description, authors) => {
+    cy.contains('Add new').click();
+    cy.get('#title').type(title);
+    cy.get('#description').type(description);
+    cy.get('#authors').type(authors);
+    cy.contains('Submit').click();
+})
+
+Cypress.Commands.add('addToFavoritesWhenAddingBook', (title, description, authors) => {
+    cy.contains('Add new').click();
+    cy.get('#title').type(title);
+    cy.get('#description').type(description);
+    cy.get('#authors').type(authors);
+    cy.get('#favorite').click();
     cy.contains('Submit').click();
 })
